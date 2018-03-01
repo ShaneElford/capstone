1a21e0134662963d9016cbaad6eae8006230603c
Timothy Bunt <n9408002@qut.edu.au>
2018-03-01 17:09:32 +1000

    Added project file

---
+++ 96five/96five/96five/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using _96five.Models;
+
+namespace _96five.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
