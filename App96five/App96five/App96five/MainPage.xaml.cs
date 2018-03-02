f2e204a0aacf7782e811047215b124e536674d65
Timothy Bunt <n9408002@qut.edu.au>
2018-03-02 20:37:04 +1000

    Completed navigation on mainpage

--- App96five/App96five/App96five/MainPage.xaml.cs
+++ App96five/App96five/App96five/MainPage.xaml.cs
@@ -18,6 +18,41 @@ namespace App96five
             await Navigation.PushAsync(new ListenNow());
         }
 
+        async void GoToSongFinder(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SongFinder());
+        }
+
+        async void GoToPodcasts(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Podcasts());
+        }
+
+        async void GoToSocialMedia(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SocialMedia());
+        }
+
+        async void GoToWhatsOn(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new WhatsOn());
+        }
+
+        async void GoToProgramGuide(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProgramGuide());
+        }
+
+        async void GoToExploringFaith(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ExploringFaith());
+        }
+
+        async void GoToDonations(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Donations());
+        }
+
         async void OnPreviousPageButtonClicked(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
