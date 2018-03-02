89fe31ec7e086247488db72133ceaaecb269d4fc
Timothy Bunt <n9408002@qut.edu.au>
2018-03-02 20:30:14 +1000

    Added remaining pages

---
+++ App96five/App96five/App96five/Podcasts.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App96five
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Podcasts : ContentPage
+	{
+		public Podcasts ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ App96five/App96five/App96five/SocialMedia.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App96five
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SocialMedia : ContentPage
+	{
+		public SocialMedia ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
