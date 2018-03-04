c35dd64dc01794d22612fadb7fde615efea8c24a
Timothy Bunt <n9408002@qut.edu.au>
2018-03-04 20:50:51 +1000

    added audio for listen now

--- App96five/App96five/App96five/ListenNow.xaml.cs
+++ App96five/App96five/App96five/ListenNow.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Plugin.MediaManager;
 
 namespace App96five
 {
@@ -16,5 +17,20 @@ namespace App96five
 		{
 			InitializeComponent ();
 		}
-	}
+
+        async void Play96Five()
+        {
+            await CrossMediaManager.Current.Play("http://stream2.cnmns.net/96five-mp3");
+        }
+
+        async void PlayInspire()
+        {
+            await CrossMediaManager.Current.Play("http://stream2.cnmns.net/inspire-mp3");
+        }
+
+        async void PlayFresh()
+        {
+            await CrossMediaManager.Current.Play("http://stream2.cnmns.net/fresh-mp3");
+        }
+    }
 }
