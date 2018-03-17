e5cabba2b28216a2ac25ad8f8cb54adb7f032219
Timothy Bunt <n9408002@qut.edu.au>
2018-03-17 13:13:13 +1000

    added icons for android

--- App96five/App96five/App96five/SocialMedia.xaml.cs
+++ App96five/App96five/App96five/SocialMedia.xaml.cs
@@ -10,6 +10,7 @@ using Xamarin.Forms.Xaml;
 namespace App96five
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
+
 	public partial class SocialMedia : ContentPage
 	{
 		public SocialMedia ()
@@ -17,9 +18,24 @@ namespace App96five
 			InitializeComponent ();
 		}
 
-        void GoToFacebook(object sender, EventArgs args)
+        void GoToFacebook(object sender, EventArgs e)
         {
-            Device.OpenUri(new Uri("fb://page/122991994390985"));
+            Device.OpenUri(new Uri("https://www.facebook.com/96five/"));
+        }
+        //fb://page/122991994390985
+        void GoToInstagram(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.instagram.com/96five/?hl=en"));
+        }
+        //instagram://user?username=1099802346
+        void GoToTwitter(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://twitter.com/96five"));
+        }
+//twitter://user?user_id=80201377
+        void GoToYoutube(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.youtube.com/channel/UCKPR63G0OnP-zi6ZswXXB1w"));
         }
     }
 }
