d45ffd4ef9d6f94087df7b77f2f47cb96aaa3c8c
Timothy Bunt <n9408002@qut.edu.au>
2018-04-29 18:05:26 +1000

    basic API integration

--- App96five/App96five/App96five/ListenNow.xaml.cs
+++ App96five/App96five/App96five/ListenNow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,16 +8,74 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Plugin.MediaManager;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace App96five
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ListenNow : ContentPage
-	{
-		public ListenNow ()
-		{
-			InitializeComponent ();
-		}
+    public class Now
+    {
+        public string time { get; set; }
+        public string title { get; set; }
+        public string artist { get; set; }
+        public string duration { get; set; }
+        public string album { get; set; }
+        public string token { get; set; }
+        public string albumart_url { get; set; }
+    }
+
+    public class Recent
+    {
+        public string time { get; set; }
+        public string title { get; set; }
+        public string artist { get; set; }
+        public string duration { get; set; }
+        public string album { get; set; }
+        public string token { get; set; }
+        public string albumart_url { get; set; }
+    }
+
+    public class Show
+    {
+        public string startTime { get; set; }
+        public string endTime { get; set; }
+        public object showAnnouncers { get; set; }
+        public string showArtwork { get; set; }
+        public string showName { get; set; }
+        public string showURL { get; set; }
+        public string day { get; set; }
+        public string token { get; set; }
+    }
+
+    public class Station
+    {
+        public Now now { get; set; }
+        public List<Recent> recent { get; set; }
+        public Show show { get; set; }
+        public int secsUntilRefresh { get; set; }
+    }
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListenNow : ContentPage
+    {
+        private const string station_96five = "http://onair.hopemedia.com.au/api/station.php?station=hope1032&recent=false";
+        private HttpClient _Client = new HttpClient();
+        private ObservableCollection<Station> _station;
+
+        public ListenNow()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            var content = await _Client.GetStringAsync(station_96five);
+            var content2 = "[" + content + "]";
+            var station = JsonConvert.DeserializeObject<List<Station>>(content2);
+            _station = new ObservableCollection<Station>(station);
+            Display_96five.ItemsSource = _station;
+            base.OnAppearing();
+        }
 
         async void Play96Five()
         {
@@ -37,5 +96,6 @@ namespace App96five
         {
             await CrossMediaManager.Current.Stop();
         }
+
     }
 }
