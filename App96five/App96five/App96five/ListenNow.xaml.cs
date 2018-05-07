a53112fc976ed332b8bd1e087a16a8cd6449a53f
Timothy Bunt <n9408002@qut.edu.au>
2018-05-08 00:09:04 +1000

    completed listen now page. added scrollview for each station, showing 6 most recent songs played.

--- App96five/App96five/App96five/ListenNow.xaml.cs
+++ App96five/App96five/App96five/ListenNow.xaml.cs
@@ -13,54 +13,34 @@ using Newtonsoft.Json;
 
 namespace App96five
 {
-    public class Now
-    {
-        public string time { get; set; }
-        public string title { get; set; }
-        public string artist { get; set; }
-        public string duration { get; set; }
-        public string album { get; set; }
-        public string token { get; set; }
-        public string albumart_url { get; set; }
-    }
-
     public class Recent
     {
+        public string body { get; set; }
         public string time { get; set; }
         public string title { get; set; }
         public string artist { get; set; }
         public string duration { get; set; }
         public string album { get; set; }
         public string token { get; set; }
-        public string albumart_url { get; set; }
-    }
-
-    public class Show
-    {
-        public string startTime { get; set; }
-        public string endTime { get; set; }
-        public object showAnnouncers { get; set; }
-        public string showArtwork { get; set; }
-        public string showName { get; set; }
-        public string showURL { get; set; }
-        public string day { get; set; }
-        public string token { get; set; }
+        public Uri albumart_url { get; set; }
     }
 
     public class Station
     {
-        public Now now { get; set; }
         public List<Recent> recent { get; set; }
-        public Show show { get; set; }
         public int secsUntilRefresh { get; set; }
     }
 
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListenNow : ContentPage
     {
-        private const string station_96five = "http://onair.hopemedia.com.au/api/station.php?station=hope1032";
+        private const string station_96five = "http://onair.hopemedia.com.au/api/station.php?station=hope1032&now=false&show=false";
+        private const string station_inspire = "http://onair.hopemedia.com.au/api/station.php?station=inspiredigital&now=false&show=false";
+        private const string station_fresh = "http://onair.hopemedia.com.au/api/station.php?station=fresh&now=false&show=false";
         private HttpClient _Client = new HttpClient();
-        private ObservableCollection<Station> _station;
+        private ObservableCollection<Station> _96fivestation;
+        private ObservableCollection<Station> _inspirestation;
+        private ObservableCollection<Station> _freshstation;
 
         public ListenNow()
         {
@@ -69,22 +49,50 @@ namespace App96five
 
         protected override async void OnAppearing()
         {
-            var content = await _Client.GetStringAsync(station_96five);
-            var content2 = "[" + content + "]";
-            var station = JsonConvert.DeserializeObject<List<Station>>(content2);
-            _station = new ObservableCollection<Station>(station);
-            now_96five.ItemsSource = _station;
+            var content_96five = await _Client.GetStringAsync(station_96five);
+            var station1 = JsonConvert.DeserializeObject<List<Station>>("[" + content_96five + "]");
+            _96fivestation = new ObservableCollection<Station>(station1);
+            now_96five.ItemsSource = _96fivestation;
+
+            var content_inspire = await _Client.GetStringAsync(station_inspire);
+            var station2 = JsonConvert.DeserializeObject<List<Station>>("[" + content_inspire + "]");
+            _inspirestation = new ObservableCollection<Station>(station2);
+            now_inspire.ItemsSource = _inspirestation;
+
+            var content_fresh = await _Client.GetStringAsync(station_fresh);
+            var station3 = JsonConvert.DeserializeObject<List<Station>>("[" + content_fresh + "]");
+            _freshstation = new ObservableCollection<Station>(station3);
+            now_fresh.ItemsSource = _freshstation;
             base.OnAppearing();
+        }
+
+        public void Select96Five()
+        {
+            now_96five.IsVisible = true;
+            now_inspire.IsVisible = false;
+            now_fresh.IsVisible = false;
         }
 
         async void Play96Five()
         {
             await CrossMediaManager.Current.Play("http://stream2.cnmns.net/96five-mp3");
         }
+        public void SelectInspire()
+        {
+            now_96five.IsVisible = false;
+            now_inspire.IsVisible = true;
+            now_fresh.IsVisible = false;
+        }
 
         async void PlayInspire()
         {
             await CrossMediaManager.Current.Play("http://stream2.cnmns.net/inspire-mp3");
+        }
+        public void SelectFresh()
+        {
+            now_96five.IsVisible = false;
+            now_inspire.IsVisible = false;
+            now_fresh.IsVisible = true;
         }
 
         async void PlayFresh()
