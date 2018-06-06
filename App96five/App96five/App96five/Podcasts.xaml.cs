b3c458479eb14a04c9018874dd3590a2f6132de8
Timothy Bunt <n9408002@qut.edu.au>
2018-06-06 13:32:21 +1000

    completed podcasts page, worth 96five making talkinglife playlist similar to what they have done with goodmorning and familyworship

--- App96five/App96five/App96five/Podcasts.xaml.cs
+++ App96five/App96five/App96five/Podcasts.xaml.cs
@@ -15,13 +15,117 @@ namespace App96five
 		public Podcasts ()
 		{
 			InitializeComponent ();
-            GoodMorning_01.Source = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/365051201&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false";
-            GoodMorning_02.Source = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/365047988&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true";
+            GoodMorningView.Source = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/284954437&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true";
+            FamilyWorshipView.Source = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/318406640&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true";
+            LifeWordsView.Source = "https://itunes.apple.com/us/podcast/lifewords-q-a-with-david-reay/id897421376?mt=2#TrackList";
+            OpenHouseView.Source = "https://itunes.apple.com/au/podcast/open-house/id1084940084?mt=2#TrackList";
+            TalkingLifeView.Source = "https://m.soundcloud.com/search/tracks?q=96five%20talking%20life";
         }
 
         public void SelectGoodMorning()
         {
-            GoodMorningView.IsVisible = true;
+            if (GoodMorningView.IsVisible == false)
+            {
+                GoodMorning.IsVisible = true;
+                GoodMorningView.IsVisible = true;
+                FamilyWorship.IsVisible = false;
+                LifeWords.IsVisible = false;
+                OpenHouse.IsVisible = false;
+                TalkingLife.IsVisible = false;
+            }
+            else
+            {
+                GoodMorning.IsVisible = true;
+                GoodMorningView.IsVisible = false;
+                FamilyWorship.IsVisible = true;
+                LifeWords.IsVisible = true;
+                OpenHouse.IsVisible = true;
+                TalkingLife.IsVisible = true;
+            }
         }
-	}
+        public void SelectFamilyWorship()
+        {
+            if (FamilyWorshipView.IsVisible == false)
+            {
+                GoodMorning.IsVisible = false;
+                FamilyWorship.IsVisible = true;
+                FamilyWorshipView.IsVisible = true;
+                LifeWords.IsVisible = false;
+                OpenHouse.IsVisible = false;
+                TalkingLife.IsVisible = false;
+            }
+            else
+            {
+                GoodMorning.IsVisible = true;
+                FamilyWorship.IsVisible = true;
+                FamilyWorshipView.IsVisible = false;
+                LifeWords.IsVisible = true;
+                OpenHouse.IsVisible = true;
+                TalkingLife.IsVisible = true;
+            }
+        }
+        public void SelectLifeWords()
+        {
+            if (LifeWordsView.IsVisible == false)
+            {
+                GoodMorning.IsVisible = false;
+                FamilyWorship.IsVisible = false;
+                LifeWords.IsVisible = true;
+                LifeWordsView.IsVisible = true;
+                OpenHouse.IsVisible = false;
+                TalkingLife.IsVisible = false;
+            }
+            else
+            {
+                GoodMorning.IsVisible = true;
+                FamilyWorship.IsVisible = true;
+                LifeWords.IsVisible = true;
+                LifeWordsView.IsVisible = false;
+                OpenHouse.IsVisible = true;
+                TalkingLife.IsVisible = true;
+            }
+        }
+        public void SelectOpenHouse()
+        {
+            if (OpenHouseView.IsVisible == false)
+            {
+                GoodMorning.IsVisible = false;
+                FamilyWorship.IsVisible = false;
+                LifeWords.IsVisible = false;
+                OpenHouse.IsVisible = true;
+                OpenHouseView.IsVisible = true;
+                TalkingLife.IsVisible = false;
+            }
+            else
+            {
+                GoodMorning.IsVisible = true;
+                FamilyWorship.IsVisible = true;
+                LifeWords.IsVisible = true;
+                OpenHouse.IsVisible = true;
+                OpenHouseView.IsVisible = false;
+                TalkingLife.IsVisible = true;
+            }
+        }
+        public void SelectTalkingLife()
+        {
+            if (TalkingLifeView.IsVisible == false)
+            {
+                GoodMorning.IsVisible = false;
+                FamilyWorship.IsVisible = false;
+                LifeWords.IsVisible = false;
+                OpenHouse.IsVisible = false;
+                TalkingLife.IsVisible = true;
+                TalkingLifeView.IsVisible = true;
+            }
+            else
+            {
+                GoodMorning.IsVisible = true;
+                FamilyWorship.IsVisible = true;
+                LifeWords.IsVisible = true;
+                OpenHouse.IsVisible = true;
+                TalkingLife.IsVisible = true;
+                TalkingLifeView.IsVisible = false;
+            }
+        }
+    }
 }
